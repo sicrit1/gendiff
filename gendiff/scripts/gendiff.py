ad09405db965216c3cabb416b1ac4a6d5c503553
sicrit1 <zapadin@gmail.com>
2022-06-23 14:47:07 +0300

    first step

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,10 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    args = parser.parse_args()
+
+if __name__ == '__main__':
+    main()
