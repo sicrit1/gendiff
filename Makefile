c24fea209c2d0222fed3220d45c14f380731d5f3
sicrit1 <zapadin@gmail.com>
2022-06-23 15:33:32 +0300

    second step

---
+++ Makefile
@@ -0,0 +1,18 @@
+install:
+	poetry install
+
+build:
+	poetry build
+
+publish:
+	poetry publish --dry-run
+
+package-install:
+	python3 -m pip install --user dist/*.whl
+
+lint:
+	poetry run flake8 brain_games
+
+uninstall-code:
+	python3 -m pip uninstall hexlet-code
+
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,9 +1,10 @@
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('first_file', type=str)
-    parser.add_argument('second_file', type=str)
+    parser = argparse.ArgumentParser(description = 'Compares two configuration files and shows a difference.' )
+    parser.add_argument('first_file', type = str)
+    parser.add_argument('second_file', type = str)
+    parser.add_argument('-f', '--format', help = 'set format of output')
     args = parser.parse_args()
 
 if __name__ == '__main__':
